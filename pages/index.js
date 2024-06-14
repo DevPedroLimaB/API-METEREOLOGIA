e1c0c575e0b328c72630a4a2bd0b8e4ccfb6850a
DevPedroLimaB <pedrohlima26@icloud.com>
2024-06-13 22:31:03 -0300

    1.0

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return <div id="Home">
+      <h1>API de Meteorologia</h1>
+      <div id="tempo-atual"></div> 
+      <div id="previsao"></div>   
+      <div id="historico"></div> 
+    </div>
+}
+
+export default Home;
